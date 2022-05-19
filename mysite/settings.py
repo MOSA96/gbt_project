81de064f78e214d45d0f360aa7493e1edb7d716c
aa.morales <aa.morales@globant.com>
2022-05-19 14:47:22 -0500

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$(ww_%nd6dt$0(axq$7akkshq(2x##_$$d#&5nwq8rtv^=ctm4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
