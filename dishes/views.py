2dedac7f3e9e55a73a2412e117921f87bcad459d
aa.morales <aa.morales@globant.com>
2022-05-25 14:26:10 -0500

    add master branch

--- dishes/views.py
+++ dishes/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import User, Dish, Ingredient
 
 def index(request):
-    return HttpResponse("Hello World.")
+    """View function for home page"""
+    num_user = User.objects.all().count()
+
+    context = {
+        'num_user': num_user,
+    }
+    return render(request, 'index.html', context=context)
