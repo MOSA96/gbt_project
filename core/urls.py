d726778f2efdc8bb6e4495a9e8e00bc98c748aa8
aa.morales <aa.morales@globant.com>
2022-06-21 16:01:31 -0500

    add nav profile reaction

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.loginview, name='login')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from dishes.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
+#Core views
 def home(request):
     """View function for home page"""
     num_user = User.objects.all().count()
@@ -18,3 +20,33 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+#Log In views
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def loginview(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            user = authenticate(request, username=username, password=password )
+            if user is not None and user.is_active and user.check_password:
+                login(request, user)
+                return redirect('home')
+            else:
+                login_errors = 'Invalid User or Password'
+                return render(request, 'login.html', {'login_erros': login_errors})
+        else:
+            if request.user and request.user.is_authenticated:
+                return redirect('home')
+
+    return render(request, 'login.html')
+
+
+def logout(request):
+    if request.user:
+        logout(request)
+    return redirect('home')
